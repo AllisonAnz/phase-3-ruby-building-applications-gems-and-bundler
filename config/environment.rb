f7179f672837bebf70ded19889bb343d0a139b8e
AllisonAnz <AllisonAnzalone@gmail.com>
2021-08-30 11:30:03 -0500

    Completed Assignment

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,12 @@
 # Set up Bundler here following the instructions in the README
+require 'bundler/setup'
+Bundler.require(:default, :development) 
+
+#  Above is equivalent to this code
+#   require 'rspec'
+#   require 'rest-client'
+#   require 'pry' 
+
+# Important: two arguments that you are passing in the #require method 
+# must be passed in the correct order, show above. 
+# The test you are trying to pass is testing for order 
